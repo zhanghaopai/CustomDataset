66a719b192b4a587c4c2a6c9ac1139390889a732
Patrick <zhanghaopai@outlook.com>
2024-07-31 22:01:19 +0800

    提交二分类代码

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CustomDataset
-Build a custom dataset
+> Build a custom dataset
+
+## Objectives
+1. Download and tokenize a IMDb review dataset.
+2. Train a model on the dataset which can predict the 
+sentiment of the review.
+
+## Steps
---
+++ dataloader.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import torch
+from sklearn.model_selection import train_test_split
+from transformers import DistilBertTokenizerFast
+
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir / label_dir).iterdir():
+            texts.append(text_file.read_text(encoding="utf8"))
+            labels.append(0 if label_dir == "neg" else 1)
+    return texts, labels
+
+
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val) for key, val in self.encodings.item()}
+        item['labels'] = torch.tensor(self.lables[idx])
+        return item
+
+
+def main():
+    train_texts, train_labels = read_imdb_split("assets/aclImdb/train")
+    test_texts, test_labels = read_imdb_split("assets/aclImdb/test")
+    # 区分训练集和验证集
+    train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=0.2)
+    # 分词器
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+    val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+    test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+    train_dataset = IMDbDataset(train_encodings, train_labels)
+    val_dataset = IMDbDataset(val_encodings, val_labels)
+    test_dataset = IMDbDataset(test_encodings, test_labels)
+    
+    print(train_dataset)
+
+
+if __name__ == '__main__':
+    main()
